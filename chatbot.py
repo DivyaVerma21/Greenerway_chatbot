6856de92653f583b0e6adbb0384257fc4188b9af
DivyaVerma21 <divya.verma2101@gmail.com>
2025-04-09 10:36:41 +0200

    resolved errors

--- chatbot.py
+++ chatbot.py
@@ -98,13 +98,14 @@ def main():
         st.session_state.messages.append({'role': 'user', 'content': prompt})
 
         # Redirect logic for specific user queries
-        if "bess size calculator" or "bess calculator" or "BESS Calculator" or "BESS Size Calculator" in prompt.lower():
+        if any(keyword in prompt.lower() for keyword in
+               ["bess size calculator", "bess calculator", "bess size", "bess"]):
             st.markdown("[Go to BESS Calculator](https://www.greenerway.no/calculator)")
             return
-        elif "schedule of day" or "scheduler" or "SCHEDULER" in prompt.lower():
+        elif any(keyword in prompt.lower() for keyword in ["schedule of day", "scheduler", "schedule", "plan"]):
             st.markdown("[Go to Scheduler App](https://scheduler-savings.streamlit.app/)")
             return
-        elif "ems" or "EMS" in prompt.lower():
+        elif "ems" in prompt.lower():
             st.markdown(
                 "[Go to EMS Dashboard](https://ems.greenerway.services/dashboard/62727b89-347c-40b0-b746-0f06ec913894)")
             return
